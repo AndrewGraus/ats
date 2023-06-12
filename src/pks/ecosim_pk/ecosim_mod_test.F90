415edf8943fc80c0578f77f7c2b02c6b637e2aa7
Andrew Graus <agraus@n0002.scs00>
2023-06-12 10:48:51 -0700

    printing size

--- src/pks/ecosim_pk/ecosim_mod_test.F90
+++ src/pks/ecosim_pk/ecosim_mod_test.F90
@@ -30,15 +30,21 @@ contains
     use, intrinsic :: iso_c_binding
     integer (c_int), value, intent(in) :: col
     type(BGCProperties) :: props
+    integer :: i
+    integer :: len
 
     write(*,*) "Okay calling the props function works."
     write(*,*) "num col is: ", col
     write(*,*) "printing the BGCProps data: "
-    do i = 1, props%volume%size
-      write(*,*) props%volume%data(i)
-    end do
+    
+    len = props%volume%size
 
-    write(*,*)
+    write(*,*) "the length is: ", len    
+    !do i = 1, props%volume%size
+    !  write(*,*) props%volume%data(i)
+    !end do
+
+    write(*,*) "the properties are finished"
 
   end subroutine ecosim_datatest
 
