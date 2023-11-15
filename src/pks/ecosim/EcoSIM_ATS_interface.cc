c1eaa54d6af7c66cc1211ebe5afa6ae34bbfb537
AndrewGraus <andrewsgraus@gmail.com>
2023-11-14 16:20:15 -0800

    trying to add suction head

--- src/pks/ecosim/EcoSIM_ATS_interface.cc
+++ src/pks/ecosim/EcoSIM_ATS_interface.cc
@@ -121,8 +121,8 @@ EcoSIM::EcoSIM(Teuchos::ParameterList& pk_tree,
     atm_n2o_ = plist_->get<double>("atmospheric N2O");
     atm_h2_ = plist_->get<double>("atmospheric H2");
     atm_nh3_ = plist_->get<double>("atmospheric NH3");
-    pressure_at_field_capacity = plist_->get<double>("Field Capacity [Mpa < 0]");
-    pressure_at_wilting_point = plist_->get<double>("Wilting Point [Mpa < 0]");
+    pressure_at_field_capacity = plist_->get<double>("Field Capacity [Mpa]");
+    pressure_at_wilting_point = plist_->get<double>("Wilting Point [Mpa]");
 
     dt_ = plist_->get<double>("initial time step", 1.);
     c_m_ = plist_->get<double>("heat capacity [J mol^-1 K^-1]");
